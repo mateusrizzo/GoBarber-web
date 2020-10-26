c645fa976b22814cafc058c2baf0da81b7dfb72a
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-26 16:14:49 -0300

    Created sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
